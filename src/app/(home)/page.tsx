a102f374ceea99a229173ab772e0f752bd1faaab
Brandon Bryan <bshwag@gmail.com>
2023-06-02 18:03:44 -0400

    Fixed folder casing

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import MainHeader from "@/components/home/mainheader/MainHeader"
-import SubHeader from "../../components/home/subheader/subheader"
+import SubHeader from "../../components/home/subheader/SubHeader"
 import Hero from "@/components/home/hero/Hero"
 import DataTableMain from "@/components/data-table/datatable"
 
