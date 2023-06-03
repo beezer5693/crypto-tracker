49b0f50df2498afc3002307f178ae0d28198b615
Brandon Bryan <bshwag@gmail.com>
2023-06-02 20:33:06 -0400

    Fixed sizing issues in header

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import DataTableMain from "@/components/data-table/datatable"
 
 export default async function Home() {
 	return (
-		<main className="flex min-h-screen flex-col bg-white pb-20 dark:bg-[#1c1c1c]">
+		<main className="flex min-h-screen flex-col overflow-x-hidden bg-white pb-20 dark:bg-[#1c1c1c]">
 			<SubHeader />
 			<MainHeader />
 			{/* @ts-expect-error Async Server Component */}
--- src/components/home/mainheader/MainHeader.tsx
+++ src/components/home/mainheader/MainHeader.tsx
@@ -16,12 +16,12 @@ export default function MainHeader() {
 	return (
 		<div className="sticky top-0 z-50 w-full">
 			<Header className="flex w-full flex-col gap-5 border-b border-neutral-200/60 bg-white px-6 py-3.5  dark:border-neutral-700/50 dark:bg-[#1c1c1c] lg:flex-row">
-				<div className="flex items-center gap-20">
+				<div className="flex flex-col items-center gap-5 sm:flex-row sm:gap-20">
 					<Logo height={20} width={20} />
 					<nav className="flex items-center">
-						<ul className="flex items-center space-x-7">
+						<ul className="flex items-center space-x-2 sm:space-x-7">
 							<li
-								className="flex cursor-pointer items-center gap-1.5 text-[.8rem] font-medium text-neutral-800 transition duration-200 ease-out hover:text-emerald-500 dark:text-neutral-100 dark:hover:text-emerald-400"
+								className="flex cursor-pointer items-center gap-0.5 text-[.7rem] font-medium text-neutral-800 transition duration-200 ease-out hover:text-emerald-500 dark:text-neutral-100 dark:hover:text-emerald-400 sm:gap-1.5 sm:text-[.8rem]"
 								onClick={() => setIsOpen(prev => !prev)}
 							>
 								<span>Cryptocurrencies</span>
@@ -30,15 +30,15 @@ export default function MainHeader() {
 									size={12}
 								/>
 							</li>
-							<li className="flex cursor-not-allowed items-center gap-1.5 text-[.8rem] font-medium text-neutral-600 transition duration-200 ease-out  dark:text-neutral-400">
+							<li className="flex cursor-not-allowed items-center gap-0.5 text-[.7rem] font-medium text-neutral-600 transition duration-200 ease-out dark:text-neutral-400 sm:gap-1.5  sm:text-[.8rem]">
 								<span>Exchanges</span>
 								<ChevronDown className={cn("stroke-neutral-400")} size={12} />
 							</li>
-							<li className="flex cursor-not-allowed items-center gap-1.5 text-[.8rem] font-medium text-neutral-600 transition duration-200 ease-out  dark:text-neutral-400">
+							<li className="flex cursor-not-allowed items-center gap-0.5 text-[.7rem] font-medium text-neutral-600 transition duration-200 ease-out dark:text-neutral-400 sm:gap-1.5  sm:text-[.8rem]">
 								<span>Community</span>
 								<ChevronDown className={cn("stroke-neutral-400")} size={12} />
 							</li>
-							<li className="flex cursor-not-allowed items-center gap-1.5 text-[.8rem] font-medium text-neutral-600 transition duration-200 ease-out  dark:text-neutral-400">
+							<li className="flex cursor-not-allowed items-center gap-0.5 text-[.7rem] font-medium text-neutral-600 transition duration-200 ease-out dark:text-neutral-400 sm:gap-1.5  sm:text-[.8rem]">
 								<span>Products</span>
 								<ChevronDown className={cn("stroke-neutral-400")} size={12} />
 							</li>
